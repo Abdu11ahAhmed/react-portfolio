766b4cf48ab150e46d5b42ddf71168f06fadf6cb
Abdu11ahAhmed <sjsuper872@gmail.com>
2025-04-23 15:10:17 +0200

    done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+};
+
+module.exports = {
+    basePath: "/page",
 };
 
 export default nextConfig;
